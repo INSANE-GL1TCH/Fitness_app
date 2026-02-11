3effc5e563bd30365f98830dcdd41175a4382b74
INSANE-GL1TCH <gautamishan81@gmail.com>
2026-02-11 21:15:22 +0545

    updated index

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const app = express();
+const { sequelize, connectDB } = require("./database/database")
+const cors = require("cors")
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
+//middleware
+app.use(express.json());
+
+
+
+//userRoutes and productRoutes
+app.use("/api/user", require('./routes/userRoutes'))
 
 app.get("/",(req,res) =>{
     res.json({message: "Welcome to the Home Page"});
 });
 
-app.listen(3000, ()=>{
-    console.log("Server is Running on http://localhost:3000");
-});
+//start server
+const startServer = async () => {
+    const PORT = process.env.PORT || 3000;
+    await connectDB();
+    await sequelize.sync({alter: true});
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+};
+
+
+startServer();
+
+
