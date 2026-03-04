8dcfde534a32c333a791fa6ab70698fe78e3a682
INSANE-GL1TCH <gautamishan81@gmail.com>
2026-03-04 10:50:51 +0545

    updated

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,30 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const { sequelize, connectDB } = require("./database/database")
 const cors = require("cors")
+
+// Import ALL your models
+const RegisterUser = require("./models/userModels"); 
+const WorkoutLog = require("./models/exeModels");
+const BMI = require("./models/bmiModels"); 
+const Meal = require("./models/mealModels");
+const Badge = require("./models/badgeModels"); // 👈 NEW: Brought in the Badge model!
+
+// Define the Relationships (Associations) explicitly!
+RegisterUser.hasMany(BMI, { foreignKey: 'userId' });
+BMI.belongsTo(RegisterUser, { foreignKey: 'userId' });
+
+RegisterUser.hasMany(Meal, { foreignKey: 'userId' });
+Meal.belongsTo(RegisterUser, { foreignKey: 'userId' });
+
+RegisterUser.hasMany(WorkoutLog, { foreignKey: 'userId' });
+WorkoutLog.belongsTo(RegisterUser, { foreignKey: 'userId' });
+
+// 👇 NEW: Tell PostgreSQL to link Badges to Users!
+RegisterUser.hasMany(Badge, { foreignKey: 'userId' });
+Badge.belongsTo(RegisterUser, { foreignKey: 'userId' });
+
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
@@ -10,9 +33,7 @@ app.use(cors({
 //middleware
 app.use(express.json());
 
-
-
-//userRoutes and productRoutes
+//userRoutes
 app.use("/api/user", require('./routes/userRoutes'))
 
 app.get("/",(req,res) =>{
@@ -23,13 +44,13 @@ app.get("/",(req,res) =>{
 const startServer = async () => {
     const PORT = process.env.PORT || 3000;
     await connectDB();
-    await sequelize.sync({alter: true});
+    
+    // This will now see the new Badge model and automatically create the table in your database!
+    await sequelize.sync({alter: true}); 
+    
     app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
     });
 };
 
-
 startServer();
-
-
